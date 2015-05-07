4c82773b5f73d0439218456d83c95ef2fd0f30eb
squest <sabdaps@gmail.com>
2015-05-07 18:30:06 +0700

    sess three

--- src/basicone/sessthree/one.clj
+++ src/basicone/sessthree/one.clj
@@ -1,1 +1,39 @@
 (ns basicone.sessthree.one)
+
+;; ayo woi
+
+;; Topic : higher order function
+
+(defn sum
+  [[x & xs]]
+  (if x (+ x (sum xs)) 0))
+
+(defn product
+  [[x & xs]]
+  (if x (* x (product xs)) 1))
+
+(defn square-all
+  [[x & xs]]
+  (if x (cons (* x x) (square-all xs)) []))
+
+(defn minus-all
+  [[x & xs]]
+  (if x (cons (- x) (minus-all xs)) []))
+
+(defn memap
+  [f [x & xs]]
+  (if x (cons (f x) (memap f xs)) []))
+
+(defn square [x] (* x x))
+(defn cube [x] (* x x x))
+
+(defn adder
+  [n]
+  #(+ n %))
+
+;;
+
+
+
+
+
