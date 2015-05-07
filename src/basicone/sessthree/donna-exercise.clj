a9ff77f57d291d46115a8a3f7777b6d48d762d5b
donnitazen <donnitazen@users.noreply.github.com>
2015-05-07 20:00:06 +0700

    Update donna-exercise.clj

--- src/basicone/sessthree/donna-exercise.clj
+++ src/basicone/sessthree/donna-exercise.clj
@@ -2,23 +2,23 @@
 
 (defn new-reduce 
   ([f coll]
-    (loop [f f [x & xs] coll res x]
+    (loop [[x & xs] coll res x]
       (if (first xs)
-        (recur f xs (f res (first xs)))
+        (recur xs (f res (first xs)))
         res)))
   ([f acc coll]
-    (loop [f f [x & xs] coll res acc]
+    (loop [[x & xs] coll res acc]
       (if x
-        (recur f xs (f res x))
+        (recur xs (f res x))
         res))))
 
 ; (max-by second []) => (max-by second [[1 2] [3 4] [0 10] [3 8]]) = [0 10]
 
 (defn mapping-stuff
   [f coll]
-  (loop [f f [x & xs] coll res {}]
+  (loop [[x & xs] coll res {}]
     (if x
-      (recur f xs (assoc res (f x) x))
+      (recur xs (assoc res (f x) x))
       res)))
 
 (defn find-max
