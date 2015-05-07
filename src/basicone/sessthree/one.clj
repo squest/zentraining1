d156ea265a095217988878c62a8349b6c453436a
squest <sabdaps@gmail.com>
2015-05-07 19:56:53 +0700

    synced

--- src/basicone/sessthree/one.clj
+++ src/basicone/sessthree/one.clj
@@ -34,6 +34,41 @@
 ;; 1. re-implement reduce (reduce #(whatever %1 %2) xs)
 ;; 2. bikin function max-by & min-by -> ex. (max-by f xs)
 
+;; ini bagian iseng
+
+(defn prime?
+  [^long p]
+  (let [lim (int (Math/sqrt p))]
+    (loop [i (int 3)]
+      (cond (> i lim) true
+            (even? i) false
+            (== 0 (rem p i)) false
+            :else (recur (+ i 2))))))
+
+(def sumprime (ref 2))
+
+(defn pprimes
+  [a b c d]
+  (dosync (pvalues (doseq [i (range 1 a)
+                           :when (prime? i)]
+                     (dosync (alter sumprime + i)))
+                   (doseq [i (range a b)
+                           :when (prime? i)]
+                     (dosync (alter sumprime + i)))
+                   (doseq [i (range b c)
+                           :when (prime? i)]
+                     (dosync (alter sumprime + i)))
+                   (doseq [i (range c d)
+                           :when (prime? i)]
+                     (dosync (alter sumprime + i))))
+          @sumprime))
+
+(defn sort-luar-dalem
+  [fg fs maps]
+  (->> (group-by fg maps)
+       (sort-by #(:somekey (key %)))
+       (map #(sort-by fs (val %)))
+       (into {})))
 
 
 
