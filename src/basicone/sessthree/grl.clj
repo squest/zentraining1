d4ec931821ed18643eca1ff6a0e92cafb3a70b69
gurielamp <guriel.amp@zeniuseducation.com>
2015-05-21 18:37:01 +0700

    ses3

--- src/basicone/sessthree/grl.clj
+++ src/basicone/sessthree/grl.clj
@@ -1,5 +1,3 @@
-(ns basicone.sessthree.grl)
-
 (defn sum
   [[x & xs]]
   (if x (+ x (sum xs)) 0))
@@ -37,28 +35,116 @@
 (defn gproductloop
   [x-product]
   (loop [[x & xs] x-product res 1]
-    (if x
+    (if x 
       (recur xs (* res x))
       res)))
 
 (defn gmaxloop
-  [max-vector]
-  (loop [[x & xs] max-vector res []]
-    (if xs
-      (if (> x xs)
-        (conj (rest res) x)
-        (recur xs))
-      res)))
+  [f max-vector]
+  (loop [[x & xs] max-vector res [(last max-vector)]]
+    (if x
+      (if (> (f x) (f (first res))) 
+        (recur xs (conj (rest res) x))
+        (recur xs res))
+      (first res))))
+
+(defn max'
+  [coll]
+  (loop [[x & xs] coll tmp 0]
+    (if x 
+      (if (> tmp x)
+        (recur xs tmp)
+        (recur xs x))
+      tmp)))
 
 ;; 
 
-;; max by (maxby second [[2 3] [6 7] [9 2]]) = [6 7]
+(defn gmaxloopg
+  [max-vector]
+  (loop [[x & xs] max-vector res [(last max-vector)]]
+    (if x
+      (if (> x (first res)) 
+        (recur xs (conj (rest res) x))
+        (recur xs res))
+      (first res))))
 
+(defn firstg
+  [[x & xs]]
+  (if x
+    (cons (first x) (firstg xs))
+    []))
+
+(defn secondg
+  [[x & xs]]
+  (if x
+    (cons (second x) (secondg xs))
+    []))
+;; max by (maxby second [[2 3] [6 7] [9 2]]) = [6 7]
+(defn grange 
+  [a b]
+  (loop [a a b b res []]
+    (if (< a b)
+      (recur (inc a) b (conj res a))
+      res)
+    ))
 
 ;; min by (minby second [[2 3] [6 7] [9 2]]) = [9 2]
-
+(defn gduplicate 
+  [j]
+  (loop [[x & xs] j res []] 
+    (if x
+      (recur xs (conj (conj res x) x))
+      res)))
 
 ;; reduce
+
+(defn gfibo
+  [fibo]
+  (loop [x fibo res [] p 1 q -1]
+    (if (< x 1)
+      res
+    (recur (dec x) (conj res p) (+ p (nth res q 0)) (inc q)))))
+
+
+(defn gyea
+  [x y]
+  (loop [x x y y res []]
+    (if (or (empty? x) (empty? y))
+    res
+    (recur (rest x) (rest y) (conj res (first x) (first y))))))
+
+;; 0! = 0
+(defn gfac
+  [x]
+  (loop [x x a 1 b 1 c [0]]
+    (if (< x 0)
+      (first c)
+      (recur (dec x) (* a b) (inc b) (conj (rest c) a)))))
+
+;; 0! = 1
+(defn gfactorial
+  [x]
+  (loop [x x a 1 b 1]
+    (if (< x 1)
+      a
+      (recur (dec x) (*' a b) (inc b)))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
