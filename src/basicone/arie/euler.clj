794d99af8a9ad73788fb9d1bf5cd05e56cceb5d5
arie.pranasakti@zeniuseducation.com <arie.pranasakti@zeniuseducation.com>
2015-06-05 22:28:23 +0700

    updates

--- src/basicone/arie/euler.clj
+++ src/basicone/arie/euler.clj
@@ -5,7 +5,7 @@
 (def mapdir "G:/Arie/Games/Counter-strike 1.6/cstrike/mapcycle/")
 (defn mapparser [a] 
     (map #(apply str %) 
-         (remove #(= (first %) \return) 
+         (remove #(or (= (first %) \return) (= (first %) \newline)) 
                  (partition-by #(or (= \return %) (= \newline %)) a))))
 (defn mapdirs [n]
   (map #(str "map" % ".txt") (range 1 (inc n))))
@@ -47,6 +47,16 @@
 
 (defn maxpalin [ls1 ls2] (apply max (filter palin? (mulcarte ls1 ls2))))
 
+;#5
+(defn factors [n coll]
+  (every? true? (map #(= (rem n %) 0) coll)))
+
+(defn p5 [coll]
+  (loop [a false
+         b 2520]
+    (if (true? a)
+      (dec b)
+      (recur (factors b coll) (inc b)))))
 
 ;#6
 (defn square [num] (* num num))
@@ -83,6 +93,18 @@
 
 (apply +' (prodig (pow 2 1000)))
 
+;#9
+(defn phyta [[c b a]]
+  (and (< a b c) (= (+ (square a) (square b)) (square c)) (= 1000 (+ a b c))))
+
+(defn pfor [n]
+  (for [n1 (range 1 (inc n))
+        n3 (range 1 n1)
+        n2 (range (inc n3) n1)]
+    [n1 n2 n3]))
+
+(defn p9 [] (reduce * (first (filter #(phyta %) (pfor 1000)))))
+
 ;#8
 (def danum "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450")
 
@@ -96,6 +118,29 @@
                  (recur (apply str (rest n)) (conj vc (multstring n)))))))
 
 (p8 danum)
+
+;#13
+(def p13nu (slurp "src/basicone/arie/p13.txt"))
+
+(defn p13 []
+  (apply str (take 10 (str (reduce + (map read-string (mapparser (p13num))))))))
+
+;#20
+
+(defn factorial [n]
+  (loop [a n
+         vc [1]]
+    (if (= a 1)
+      vc
+      (recur (dec a) (cons a vc)))))
+
+(defn rnf [n]
+  (reduce * (factorial n)))
+
+(defn pisdig [n]
+  (map read-string (map str (str n))))
+
+(defn p20 [] (reduce + (pisdig (rnf 100N))))
 
 ;#22
 (defn names [] (sort (read-string (slurp "src/basicone/arie/names.txt"))))
@@ -123,3 +168,19 @@
     (if (= (dignum (last vc)) n)
       (+ (count vc) 1)
       (recur n n2 (+ n1 n2) (conj vc (+ n1 n2))))))
+
+;#28
+(defn spiralvar [n]
+  (sort (flatten (take 4 (repeat (range 2 (inc n) 2))))))
+
+(defn diagspi [n]
+  (loop [vc [1]
+         pv (spiralvar n)]
+    (if (empty? pv)
+      vc
+      (recur (cons (+ (first vc) (first pv)) vc) (rest pv)))))
+
+(defn p28 []
+  (reduce + (diagspi 1001)))
+
+
