6b8ad10ab2d1aeb18b4b1b7ef31f9f6b2de5e232
dsapoetra <angga.dimassaputra@gmail.com>
2015-06-25 18:56:33 +0700

    adding 4clojure

--- src/basicone/dsapoetra/euler/p25.clj
+++ src/basicone/dsapoetra/euler/p25.clj
@@ -1,5 +1,4 @@
 (ns basicone.dsapoetra.euler.p25)
-
 ;;util
 (defn factors [n]
   (filter #(zero? (rem n %)) (range 1 (inc n))))
@@ -8,6 +7,19 @@
   (= 2 (reduce +
                (for [i (range 1 (inc n))]
                  (if (= 0 (mod n i)) 1 0)))))
+
+(defn notprime? [n]
+  (not= 2 (reduce +
+               (for [i (range 1 (inc n))]
+                 (if (= 0 (mod n i)) 1 0)))))
+
+(defn prime2? [n]
+  (if (and (or (= 0 (rem n 2)) (= 0 (rem n 3)) (= 0 (rem n 5)) (= 0 (rem n 7))) (not= n 2) (not= n 3) (not= n 5) (not= n 7))
+    false
+    true))
+
+(defn prime3? [n]
+  (= 2 (count (factors n))))
 
 
 (defn palindrome? [n]
@@ -19,6 +31,32 @@
 (defn power [x y]
   (if (= y 1)
     x (*' x (power x (dec y)))))
+
+
+(def triangle-nums (map first (iterate (fn [[n m]] [(+ n m) (+ m 1)]) [1 2])))
+
+(defn prime-factors-of [num]
+  "Returns a sorted list of prime factors of num, including multiplicity."
+  (let [q (Math/sqrt num)
+        factor? (fn [nom den] (zero? (rem nom den)))]
+    (loop [n num
+           d 2
+           r []]
+      (cond
+        (> d q) (concat r [n])
+        (= n d) (concat r [n])
+        (factor? n d) (recur (/ n d) d (conj r d))
+        true          (recur n (inc d) r)))))
+
+(defn num-divisors-fast [num]
+  (let [freqs (reduce #(assoc %1 %2 (inc (get %1 %2 0)))
+                      {} (prime-factors-of num))]
+    (reduce #(* %1 (inc %2)) 1 (vals freqs))))
+
+(defn euler-12-fast [divisors]
+  (first (drop-while #(> divisors (num-divisors-fast %)) triangle-nums)))
+
+;; End of utils
 
 ;;1
 (defn kelipatan []
@@ -62,4 +100,47 @@
 
 ;;7
 (defn first-n-prime [x]
-  (take x (filter prime? (range 1 Integer/MAX_VALUE))))
+  (take x (filter prime3? (range 1 Integer/MAX_VALUE))))
+
+
+;;9
+(defn pythagorean? [a b c]
+  (if (= (+ (* a a) (* b b)) (* c c)) true false))
+
+(defn thousand-pythagorean [x]
+  (for [a (range 1 x)
+        b (range a x)
+        c (range (- x a b) x)
+        :when (pythagorean? a b c)]
+    (* a b c)))
+
+;;10
+(defn two-mi-prime []
+  (reduce + (take-while #(> 2000000 %) (filter prime? (range 1 Integer/MAX_VALUE)))))
+
+(defn max-n-prime [x y]
+  (take x (filter prime? (range y Integer/MAX_VALUE))))
+
+(defn lazy-primes-cgrande []
+  (letfn [(enqueue [sieve n step]
+                   (let [m (+ n step)]
+                     (if (sieve m)
+                       (recur sieve m step)
+                       (assoc sieve m step))))
+          (next-sieve [sieve n]
+                      (if-let [step (sieve n)]
+                        (-> sieve
+                            (dissoc n)
+                            (enqueue n step))
+                        (enqueue sieve n (+ n n))))
+          (next-primes [sieve n]
+                       (if (sieve n)
+                         (recur (next-sieve sieve n) (+ n 2))
+                         (cons n (lazy-seq (next-primes (next-sieve sieve n) (+ n 2))))))]
+    (cons 2 (lazy-seq (next-primes {} 3)))))
+
+;;12
+
+(= (last ((fn first-n-prime [x]
+            (take x (filter #(= 2 (reduce + (for [i (range 1 (inc %))](if (= 0 (mod % i)) 1 0)))) (range 1 Integer/MAX_VALUE)))) 100)) 541)
+
