0f63963e3744104495716a4424b37c9374f9d3cf
squest <sabdaps@gmail.com>
2015-06-26 08:10:34 +0700

    just sync

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject basicone "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.7.0-alpha6"]]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [com.taoensso/carmine "2.11.1"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [basicone.core]
   :main basicone.core)
