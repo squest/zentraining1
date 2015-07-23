6a1309176495fff1dbd9f0b71eeef9b2c6768712
squest <sabdaps@gmail.com>
2015-07-23 21:57:18 +0700

    sieve examples

--- src/basicone/sfive/one.clj
+++ src/basicone/sfive/one.clj
@@ -1,15 +1,18 @@
 (ns basicone.sfive.one)
 ;; sieve
+
 (defn sieve1
-  [lim]
+  [^long lim]
   (let [primes (boolean-array (+ lim 1) true)
-        res (atom [2])]
+        res (atom (int 2))]
     (do (doseq [i (range 3 (+ lim 1) 2)
                 :when (aget primes i)]
           (do (doseq [j (range (* i i) (+ lim 1) (* 2 i))]
                 (aset primes j false))
-              (swap! res #(conj @res i))))
+              (swap! res #(+ % i))))
         @res)))
+
+
 
 (defn sieve
   [lim]
@@ -27,3 +30,38 @@
                 (recur (+ i 2) (conj! res i)))
             (recur (+ i 2) (conj! res i)))
           (recur (+ i 2) res))))))
+
+(defn jumlah-faktor
+  [^long lim]
+  (let [faks (int-array (+ lim 1) 1)
+        llim (int (Math/sqrt lim))]
+    (do (doseq [i (range 2 (+ llim 1))]
+          (let [isqr (* i i)]
+            (doseq [j (range (* 2 i) (+ lim 1) i)
+                    :when (<= isqr j)]
+              (if (== (* i i) j)
+                (aset faks j (+ i (aget faks j)))
+                (aset faks j (+ i (quot j i) (aget faks j)))))))
+        (sequence
+          (comp (map #(vector % (aget faks %)))
+                (filter #(let [[a b] %] (> b a)))
+                (map first))
+          (range 2 (+ lim 1))))))
+
+(defn sol23
+  [^long lim]
+  (let [tmp (int-array (jumlah-faktor lim))
+        cnt (count tmp)
+        abuns (boolean-array (+ lim 1) false)]
+    (do (doseq [i (range cnt)
+                :let [itmp (aget tmp i)]]
+          (doseq [j (range i cnt)
+                  :let [jtmp (aget tmp j)
+                        stmp (+ itmp jtmp)]
+                  :while (<= stmp lim)]
+            (aset abuns stmp true)))
+        (->> (range 12 (+ lim 1))
+             (filter #(aget abuns %))
+             (reduce +)
+             (- (reduce + (range (+ lim 1))))))))
+
